e5dd897b243ff10136b9279e297e8a85f6b74e6a
ClementJouan <34087825+ClementJouan@users.noreply.github.com>
2020-01-23 14:03:50 +0100

    Update TestProjet.java

--- src/test/java/TestProjet.java
+++ src/test/java/TestProjet.java
@@ -4,10 +4,11 @@ import org.junit.*;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class ScenarioComplexe {
+public class TestProjet {
     private WebDriver driver;
     private String baseUrl;
     private boolean acceptNextAlert = true;
@@ -17,54 +18,53 @@ public class ScenarioComplexe {
     public void setUp() throws Exception {
         String s = System.setProperty("webdriver.gecko.driver", "C:\\firefoxDriver\\geckodriver.exe");
         driver = new FirefoxDriver();
+
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
     @Test
-    public void testScenarioComplexe() throws Exception {
-        driver.get("http://10.131.1.32/dolibarr-10.0.6/dolibarr-10.0.6/htdocs/index.php");
+    public void testProjet() throws Exception {
+driver.get("http://10.131.1.32/dolibarr-10.0.6/dolibarr-10.0.6/htdocs/index.php");
         driver.findElement(By.id("username")).clear();
         driver.findElement(By.id("username")).sendKeys("test");
-        driver.findElement(By.id("password")).click();
         driver.findElement(By.id("password")).clear();
         driver.findElement(By.id("password")).sendKeys("test");
-        driver.findElement(By.xpath("//input[@value='  Se connecter  ']")).click();
-        driver.findElement(By.xpath("//li[@id='mainmenutd_companies']/div/a/div")).click();
-        driver.findElement(By.linkText("Nouveau tiers")).click();
+        driver.findElement(By.xpath("//input[@class='button']")).click();
+        driver.findElement(By.linkText("Utilisateurs & Groupes")).click();
+        driver.findElement(By.linkText("Nouvel utilisateur")).click();
+        driver.findElement(By.id("lastname")).clear();
+        driver.findElement(By.id("lastname")).sendKeys("new");
+        driver.findElement(By.name("login")).click();
+        driver.findElement(By.name("login")).clear();
+        driver.findElement(By.name("login")).sendKeys("new");
+        driver.findElement(By.name("password")).click();
+
+        // ERROR: Caught exception [ERROR: Unsupported command [doubleClick | name=password | ]]
+        driver.findElement(By.name("password")).clear();
+        driver.findElement(By.name("password")).sendKeys("new");
+        driver.findElement(By.id("admin")).click();
+        driver.findElement(By.id("admin")).click();
+        driver.findElement(By.name("create")).click();
+        driver.findElement(By.linkText("Liste des utilisateurs")).click();
+        driver.findElement(By.xpath("//a[@id='mainmenua_commercial']/span")).click();
+        driver.findElement(By.linkText("Nouvelle commande")).click();
+        driver.findElement(By.xpath("//div[@id='id-right']/div/form/div[2]/table/tbody/tr[3]/td[2]/a/span")).click();
         driver.findElement(By.id("name")).clear();
-        driver.findElement(By.id("name")).sendKeys("banga");
+        driver.findElement(By.id("name")).sendKeys("Nike");
         driver.findElement(By.id("customerprospect")).click();
         new Select(driver.findElement(By.id("customerprospect"))).selectByVisibleText("Client");
-        driver.findElement(By.id("customerprospect")).click();
-        driver.findElement(By.id("customerprospect")).click();
-        new Select(driver.findElement(By.id("customerprospect"))).selectByVisibleText("Prospect");
         driver.findElement(By.id("customerprospect")).click();
         driver.findElement(By.name("create")).click();
-        try {
-            assertEquals(driver.findElement(By.xpath("//div[@class='fichehalfleft']//tr[1]//td[2]")).getText(), "Prospect");
-        } catch (Error e) {
-            verificationErrors.append(e.toString());
-        }
-        //div[@class='fichehalfleft']//tr[1]//td[2]
-        driver.findElement(By.linkText("MODIFIER")).click();
-        driver.findElement(By.id("customerprospect")).click();
-        new Select(driver.findElement(By.id("customerprospect"))).selectByVisibleText("Client");
-        driver.findElement(By.id("customerprospect")).click();
-        driver.findElement(By.name("save")).click();
-        driver.findElement(By.xpath("//li[@id='mainmenutd_commercial']/div/a/div")).click();
-        driver.findElement(By.linkText("Nouvelle commande")).click();
-
+        driver.findElement(By.xpath("//div[@id='id-right']/div/form/div[2]/table/tbody/tr[4]/td[2]/span/span/span/span[2]/b")).click();
+        driver.findElement(By.xpath("//div[@id='id-right']/div/form/div[2]/table/tbody/tr[4]/td[2]/span/span/span/span[2]/b")).click();
         driver.findElement(By.name("bouton")).click();
-        try {
-            assertEquals(driver.findElement(By.xpath("//div[contains(text(),\"Le champ 'Client' est obligatoire\")]")).getText(), "Le champ 'Client' est obligatoire");
-        } catch (Error e) {
-            verificationErrors.append(e.toString());
-        }
-    }
+        driver.findElement(By.linkText("Liste")).click();
 
+    }
+    
     @After
     public void tearDown() throws Exception {
-       // driver.quit();
+        driver.quit();
         String verificationErrorString = verificationErrors.toString();
         if (!"".equals(verificationErrorString)) {
             fail(verificationErrorString);
